449171820a1cc27a3ecc5d5d839fd3e650cb01cb
cab222 <cabkata@gmail.com>
2012-09-18 08:04:18 -0400

    pluralize has many attribute

--- app/models/mwod_post.rb
+++ app/models/mwod_post.rb
@@ -1,14 +1,14 @@
 class MwodPost < ActiveRecord::Base
   validates :title, :category,  :presence => true
   validates :title, :uniqueness => true
-  has_many :mwod_video_link
-  accepts_nested_attributes_for :mwod_video_link, :reject_if => lambda{ |a| a[:link].blank? }, :allow_destroy => true
-  attr_accessible :category, :date, :title, :mwod_video_link_attributes
+  has_many :mwod_video_links
+  accepts_nested_attributes_for :mwod_video_links, :reject_if => lambda{ |a| a[:link].blank? }, :allow_destroy => true
+  attr_accessible :category, :date, :title, :mwod_video_links_attributes
   before_destroy :ensure_not_referenced_by_mwod_post
   
   private
   def ensure_not_referenced_by_mwod_post
-    if mwod_video_link.empty?
+    if mwod_video_links.empty?
       return true
     else
       errors.add(:base, 'has mwod video links, delete first')
