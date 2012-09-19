7270e6ec1b44d79e059994b9d08f776774c4dab1
cab222 <cabkata@gmail.com>
2012-09-18 21:42:02 -0400

    100 % unit coverage

--- test/unit/mwod_tag_test.rb
+++ test/unit/mwod_tag_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class MwodTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validation" do
+    tag = MwodTag.new
+    assert tag.invalid?, "empty tag must be invalid"
+    assert tag.errors[:tag].any?, "empty tag has errors"
+  end
 end
--- test/unit/mwod_video_link_test.rb
+++ test/unit/mwod_video_link_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class MwodVideoLinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validation" do
+    videoLink = MwodVideoLink.new
+    assert videoLink.invalid?, "empty videoLink must be invalid"
+    assert videoLink.errors[:link].any?, "empty videoLink link has errors"
+  end
 end
