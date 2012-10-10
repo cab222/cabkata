4b7ef5c8fbcc8ce87dea67b61c02a3e75605bea6
cab222 <cabkata@gmail.com>
2012-10-10 16:50:28 -0400

    bundle install

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'PeS2cYkhHAn53KBCwceE3Q', 'A4JrkehH20eoalPIwWRl16dq2ZxeKcoXTTDZfKY8M'
-  # provider :facebook, 'APP_ID', 'APP_SECRET'
-  # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
